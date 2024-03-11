3028f77b6fae71ab191913cd608b263275792d09
Roman Makhnev <romanmahnef777@gmail.com>
2024-03-11 17:07:50 +0400

    Complete task

--- mymarket/urls.py
+++ mymarket/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from .views import BlogPostListView, BlogPostDetailView, BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView, ContactView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-
+    path('posts/', BlogPostListView.as_view(), name='blogpost_list'),
+    path('posts/<int:pk>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
+    path('posts/create/', BlogPostCreateView.as_view(), name='blogpost_create'),
+    path('posts/<int:pk>/update/', BlogPostUpdateView.as_view(), name='blogpost_update'),
+    path('posts/<int:pk>/delete/', BlogPostDeleteView.as_view(), name='blogpost_delete'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
 ]
--- mymarket/views.py
+++ mymarket/views.py
@@ -1,61 +1,45 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from django.urls import reverse
 from django.utils.text import slugify
 from .models import BlogPost
 
-     class ContactView(TemplateView):
-         template_name = 'contacts.html'
+class ContactView(TemplateView):
+    template_name = 'contacts.html'
 
-     class BlogPostListView(ListView):
-         model = BlogPost
-         template_name = 'blogpost_list.html'
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blogpost_list.html'
 
-     class BlogPostDetailView(DetailView):
-         model = BlogPost
-         template_name = 'blogpost_detail.html'
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blogpost_detail.html'
+    queryset = BlogPost.objects.filter(is_published=True)
 
-     class BlogPostCreateView(CreateView):
-         model = BlogPost
-         template_name = 'blogpost_form.html'
-         fields = ['title', 'slug', 'content', 'preview_image', 'is_published', 'views_count']
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
 
-     class BlogPostUpdateView(UpdateView):
-         model = BlogPost
-         template_name = 'blogpost_form.html'
-         fields = ['title', 'slug', 'content', 'preview_image', 'is_published', 'views_count']
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blogpost_form.html'
+    fields = ['title', 'content']
 
-     class BlogPostDeleteView(DeleteView):
-         model = BlogPost
-         template_name = 'blogpost_confirm_delete.html'
+    def form_valid(self, form):
+        new_blog = form.save(commit=False)
+        new_blog.slug = slugify(new_blog.title)
+        new_blog.save()
+        return super().form_valid(form)
 
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blogpost_form.html'
+    fields = ['title', 'content']
 
-     class BlogPostDetailView(DetailView):
-         model = BlogPost
-         template_name = 'blogpost_detail.html'
-         queryset = BlogPost.objects.filter(is_published=True)
+    def get_success_url(self):
+        return reverse('blogpost_detail', args=(self.object.pk,))
 
-         def get_object(self, queryset=None):
-             obj = super().get_object(queryset)
-             obj.views_count += 1
-             obj.save()
-             return obj
-
-
-    class BlogPostCreateView(CreateView):
-        model = BlogPost
-        template_name = 'blogpost_form.html'
-        fields = ['title', 'content']
-
-        def form_valid(self, form):
-            new_blog = form.save(commit=False)
-            new_blog.slug = slugify(new_blog.title)
-            new_blog.save()
-            return super().form_valid(form)
-
-    class BlogPostUpdateView(UpdateView):
-        model = BlogPost
-        template_name = 'blogpost_form.html'
-        fields = ['title', 'content']
-
-        def get_success_url(self):
-            return reverse('blogpost_detail', args=(self.object.pk,))
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blogpost_confirm_delete.html'
